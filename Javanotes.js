89ba8b0094c7d37791b20325617d9802f9a25205
PichikuntlaSriCharan <pichikuntlasricharan@gmail.com>
2025-08-03 09:31:26 +0530

    Started DOM

--- Javanotes.js
+++ Javanotes.js
@@ -1,269 +1,332 @@
-// 1. Variables and data types
-name="Ironman";                  //camel case is fullName
-age=20;                          // = assignment operator
-console.log(name);
-console.log("age");
-x=null;
-y=undefined;
-price=100;
-console.log(x);
-console.log(y);
-console.log("price");
-isfollow=false                 //Variables have rules(no scace between,case sensitive,no reserved names,firts letter can be $,first is letter or $ or _)
-console.log(isfollow);
-//Dynamically typed language
-name=100
-console.log(name);
-//let(once created can be updated,not redeclared),const(once created can not be updated,not redeclared)(PI=3.21),var(old script)             //defining variables
-//Const should be initialized first (const a = 10)
-//datatypes 1.primitive(number,string,boolen,undefined,null) 2.non-primitive(objects,arrays)
+// // 1. Variables and data types
+// name="Ironman";                  //camel case is fullName
+// age=20;                          // = assignment operator
+// console.log(name);
+// console.log("age");
+// x=null;
+// y=undefined;
+// price=100;
+// console.log(x);
+// console.log(y);
+// console.log("price");
+// isfollow=false                 //Variables have rules(no scace between,case sensitive,no reserved names,firts letter can be $,first is letter or $ or _)
+// console.log(isfollow);
+// //Dynamically typed language
+// name=100
+// console.log(name);
+// //let(once created can be updated,not redeclared),const(once created can not be updated,not redeclared)(PI=3.21),var(old script)             //defining variables
+// //Const should be initialized first (const a = 10)
+// //datatypes 1.primitive(number,string,boolen,undefined,null) 2.non-primitive(objects,arrays)
 
-//2. Operators
-//Arthematic operators
-let a = 1;
-let b = 2;
-let c = 3;
-console.log("a = ",a);
-console.log("b = ",b);
-console.log("a + b = ", a+b);
-console.log("a - b",a-b);
-console.log("a * b",a*b);
-console.log("a / b",a/b);
-console.log("a % b",a%b);
-console.log("a ** b",a**b);
-//unary operators
-console.log("a++",a++);            //Prer and Post increment
-console.log("a = ",a);
-console.log("a--",a--);
-console.log("a = ",a);
-console.log("++a",++a);
-console.log("a = ",a);
-console.log("--a",--a);
-console.log("a = ",a);
+// //2. Operators
+// //Arthematic operators
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// console.log("a = ",a);
+// console.log("b = ",b);
+// console.log("a + b = ", a+b);
+// console.log("a - b",a-b);
+// console.log("a * b",a*b);
+// console.log("a / b",a/b);
+// console.log("a % b",a%b);
+// console.log("a ** b",a**b);
+// //unary operators
+// console.log("a++",a++);            //Prer and Post increment
+// console.log("a = ",a);
+// console.log("a--",a--);
+// console.log("a = ",a);
+// console.log("++a",++a);
+// console.log("a = ",a);
+// console.log("--a",--a);
+// console.log("a = ",a);
 
-//Assignment opeartors                  // =,+=,-=,*=,%=,**=
-console.log("Assignment operators");
-a+=4
-console.log("a = ",a);
+// //Assignment opeartors                  // =,+=,-=,*=,%=,**=
+// console.log("Assignment operators");
+// a+=4
+// console.log("a = ",a);
 
-//Comparison operators                // ==,===,!=,!==,>,<,>=,<=
-let d=5;
-let e="5";
-console.log("d==e : ",e==d);
-console.log("d===e : ",e===d);
-console.log("d!=e : ",e!=d);
-console.log("d!=e : ",e!==d);
+// //Comparison operators                // ==,===,!=,!==,>,<,>=,<=
+// let d=5;
+// let e="5";
+// console.log("d==e : ",e==d);
+// console.log("d===e : ",e===d);
+// console.log("d!=e : ",e!=d);
+// console.log("d!=e : ",e!==d);
 
-//Logical operators                  // Logical and &&
-//logical or                         // LogicaL or ||
-//Logical nnot                       // Logical not !
+// //Logical operators                  // Logical and &&
+// //logical or                         // LogicaL or ||
+// //Logical nnot                       // Logical not !
 
-g=1;
-f=2;
-let cond1 = g < f;
-let cond2 = g!==f;
-console.log("cond1 && cond2 = ", cond1 && cond2);
-console.log("cond1 || cond2 = ", cond1 || cond2);
-console.log("!cond2 = ", !cond2);
-
-
-let cond3 = g > f;
-let cond4 = g===f;
-console.log("cond3 && cond4 = ", cond3 && cond4);
-console.log("cond3 || cond4 = ", cond3 || cond4);
-console.log("!cond3 = ",!cond3 );
+// g=1;
+// f=2;
+// let cond1 = g < f;
+// let cond2 = g!==f;
+// console.log("cond1 && cond2 = ", cond1 && cond2);
+// console.log("cond1 || cond2 = ", cond1 || cond2);
+// console.log("!cond2 = ", !cond2);
 
 
-let cond5 = g <= f;
-let cond6 = g===f;
-console.log("cond5 && cond6 = ", cond5 && cond6);
-console.log("cond5 || cond6 = ", cond5 || cond6);
-console.log("!cond6 = ", !cond6);
-
-//Conditional statements
-//1. if statement
-let agee=5;
-if(agee >= 18) {
-    console.log("Can vote");
-}
-
-if(agee < 18) {
-    console.log("Can not vote");
-}
+// let cond3 = g > f;
+// let cond4 = g===f;
+// console.log("cond3 && cond4 = ", cond3 && cond4);
+// console.log("cond3 || cond4 = ", cond3 || cond4);
+// console.log("!cond3 = ",!cond3 );
 
 
-//2. If-else statement
-let mode="dark";
- if(mode == "dark"){
-    color="black";
- }
- else {
-    color="White";
- }
- console.log(color)
- // syntac --> rules
+// let cond5 = g <= f;
+// let cond6 = g===f;
+// console.log("cond5 && cond6 = ", cond5 && cond6);
+// console.log("cond5 || cond6 = ", cond5 || cond6);
+// console.log("!cond6 = ", !cond6);
 
- //3. else-if statement
-ag=100;
-if(ag<18){
-    console.log("Junior");
-}
-else if(ag==18){
-    console.log("middle");
-}
-else{
-    console.log("Senior");
-}
-//Ternary operator
-//condition?true:false
-let ageee=18;
-let result = ageee>=18?"adult":"not adult";
-console.log(result);
-
-//alert("Hello");
-//prompt("Promt");
-
-//3.Loops
-//1.for loop
-let z=5;
-let s=0;
-for(let i=1;i<=5;i++){
-    s+=i;
-    console.log(s);
-}
-
-//2.while loop
-let i = 1;
-while (i<=10){
-    console.log(i);
-    i++;
-}
-
-//3.do-while                  //1 time min executed
-let yy=20;
-do{
-    console.log(yy);
-    yy++;
-}while(yy<=10);
-
-//4. for-of loop for strings and arrays
-tony="SriCharan";
-let size=0;
-for (let i of tony){
-    console.log(i);
-    size++;
-}
-console.log(size);
-
-//5. For-in loops         //For objects and arrays
-const student = {
-    name:"Starc",
-    age:25,
-    id:"@mail.com",
-}
-for(let key in student){
-    console.log("Key : ",key,"   Value : ",student[key]);
-}
-//-------------------------------------------------------
-//Strings
-let str="Apna \nCollage";       ///n next line;/t tab space
-console.log(str);
-let strr="Sri/tcharan";
-console.log(strr.length);            //string property
-console.log(str[0]);
-let obj = {
-    itemm : "Pen",
-    pricee  : 10,
-};
-let out='the cost of  ${obj.itemm} is ${obj.pricee} rupees';    //string interpolation
-console.log(out);
-console.log(str.toUpperCase());          //string method
-console.log(str); 
-console.log(str.toLowerCase());
-iii="             a n i t h   a            "
-console.log(iii.trim());
-//String methods
-//str.slice(start,end);
-//str1.concat(str2);
-//str.replace(searchval,newval);,replaceAll
-//str.charAt(idx);
-
-//Arrays
-let scoree=[1,2,3,44,59];
-console.log(scoree);
-console.log(scoree.length);
-for(let i=0;i<=scoree.length;i++){
-    console.log(scoree[i]);
-}
-
-//string is immutabble 
-//Array is mutable
-
-//push(), pop() and toString() methods
-let fooditems=["Potato","apple","litchi","Tomato"];
-console.log(fooditems);
-fooditems.push("chips");      // adds last
-console.log(fooditems);
-fooditems.pop();25            // Dels last
-console.log(fooditems);
-console.log(fooditems.toString());
-console.log(fooditems);
-
-marksss = [85,97,44,37,77,76,60];
-// console.log(marksss.toString());
-
-//concat,Unshift,shift methods
-
-let concatedarray = fooditems.concat(scoree,marksss);
-console.log(concatedarray);
-
-concatedarray.unshift("Antman");
-console.log(concatedarray);             //adds at last
-
-dele=concatedarray.shift();            //return first item
-console.log(dele);
-//Methods start with small letters
-
-//slice and splice
-// slice(startidx,endidx);
-// splice(startidx,delcount,newelement);
-
-//Functions
-//Function definition            and              //Function calling/invoke
-// function functionName(param1,param2){           functionName();
-//     work
-// }
-//parameters are alive till block 
-function myFun(msg){
-    console.log(msg);
-}
-myFun("I love JS");
-
-//Sum of 2 numbers
-function sum(a,b){
-    console.log(a+b);
-}
-sum(1,2)
-sum(11,26)
-
-//Function can also return a value
-function summ(a,b){
-    s=a+b;
-    console.log("Start");
-    return s;
-    console.log("end");
-}
-
-let val=summ(30,20);
-console.log(val);
-
-//Arrow function for small lines
-// const fun_name=(param1,param32222)=>{
-//     Work
+// //Conditional statements
+// //1. if statement
+// let agee=5;
+// if(agee >= 18) {
+//     console.log("Can vote");
 // }
 
-const arrowsum = (a,b) =>{
-    console.log(a+b);
-};
+// if(agee < 18) {
+//     console.log("Can not vote");
+// }
 
-arrowsum(3,4);
+
+// //2. If-else statement
+// let mode="dark";
+//  if(mode == "dark"){
+//     color="black";
+//  }
+//  else {
+//     color="White";
+//  }
+//  console.log(color)
+//  // syntac --> rules
+
+//  //3. else-if statement
+// ag=100;
+// if(ag<18){
+//     console.log("Junior");
+// }
+// else if(ag==18){
+//     console.log("middle");
+// }
+// else{
+//     console.log("Senior");
+// }
+// //Ternary operator
+// //condition?true:false
+// let ageee=18;
+// let result = ageee>=18?"adult":"not adult";
+// console.log(result);
+
+// //alert("Hello");
+// //prompt("Promt");
+
+// //3.Loops
+// //1.for loop
+// let z=5;
+// let s=0;
+// for(let i=1;i<=5;i++){
+//     s+=i;
+//     console.log(s);
+// }
+
+// //2.while loop
+// let i = 1;
+// while (i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// //3.do-while                  //1 time min executed
+// let yy=20;
+// do{
+//     console.log(yy);
+//     yy++;
+// }while(yy<=10);
+
+// //4. for-of loop for strings and arrays
+// tony="SriCharan";
+// let size=0;
+// for (let i of tony){
+//     console.log(i);
+//     size++;
+// }
+// console.log(size);
+
+// //5. For-in loops         //For objects and arrays
+// const student = {
+//     name:"Starc",
+//     age:25,
+//     id:"@mail.com",
+// }
+// for(let key in student){
+//     console.log("Key : ",key,"   Value : ",student[key]);
+// }
+// //-------------------------------------------------------
+// //Strings
+// let str="Apna \nCollage";       ///n next line;/t tab space
+// console.log(str);
+// let strr="Sri/tcharan";
+// console.log(strr.length);            //string property
+// console.log(str[0]);
+// let obj = {
+//     itemm : "Pen",
+//     pricee  : 10,
+// };
+// let out='the cost of  ${obj.itemm} is ${obj.pricee} rupees';    //string interpolation
+// console.log(out);
+// console.log(str.toUpperCase());          //string method
+// console.log(str); 
+// console.log(str.toLowerCase());
+// iii="             a n i t h   a            "
+// console.log(iii.trim());
+// //String methods
+// //str.slice(start,end);
+// //str1.concat(str2);
+// //str.replace(searchval,newval);,replaceAll
+// //str.charAt(idx);
+
+// //Arrays
+// let scoree=[1,2,3,44,59];
+// console.log(scoree);
+// console.log(scoree.length);
+// for(let i=0;i<=scoree.length;i++){
+//     console.log(scoree[i]);
+// }
+
+// //string is immutabble 
+// //Array is mutable
+
+// //push(), pop() and toString() methods
+// let fooditems=["Potato","apple","litchi","Tomato"];
+// console.log(fooditems);
+// fooditems.push("chips");      // adds last
+// console.log(fooditems);
+// fooditems.pop();25            // Dels last
+// console.log(fooditems);
+// console.log(fooditems.toString());
+// console.log(fooditems);
+
+// marksss = [85,97,44,37,77,76,60];
+// // console.log(marksss.toString());
+
+// //concat,Unshift,shift methods
+
+// let concatedarray = fooditems.concat(scoree,marksss);
+// console.log(concatedarray);
+
+// concatedarray.unshift("Antman");
+// console.log(concatedarray);             //adds at last
+
+// dele=concatedarray.shift();            //return first item
+// console.log(dele);
+// //Methods start with small letters
+
+// //slice and splice
+// // slice(startidx,endidx);
+// // splice(startidx,delcount,newelement);
+
+// //Functions-----------------
+// //Function definition            and              //Function calling/invoke
+// // function functionName(param1,param2){           functionName();
+// //     work
+// // }
+// //parameters are alive till block 
+// function myFun(msg){
+//     console.log(msg);
+// }
+// myFun("I love JS");
+
+// //Sum of 2 numbers
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(1,2)
+// sum(11,26)
+
+// //Function can also return a value
+// function summ(a,b){
+//     s=a+b;
+//     console.log("Start");
+//     return s;
+//     console.log("end");
+// }
+
+// let val=summ(30,20);
+// console.log(val);
+
+// //Arrow function for small lines
+// // const fun_name=(param1,param32222)=>{
+// //     Work
+// // }
+
+// const arrowsum = (a,b) =>{
+//     console.log(a+b);
+// };
+
+// arrowsum(3,4);
+
+//For each method
+// let arr=[1,2,3,4,5];
+// arr.forEach(function printval(val){
+//     console.log(val);
+// })
+
+//arrow function for forEach           //Arrow function is not for strings
+// let arr=["MAN","Delhi","hello"];
+// arr.forEach((val)=> {
+//     console.log(val.toUpperCase());
+// })
+
+//Arrrow function can take 3 parameters
+// let arr=["man","d elhi","hello"];
+// arr.forEach((val,idx,arr)=> {
+//     console.log(val.toUpperCase(),idx,arr);
+// })
+
+
+//Higher order functions are functions they either return their a function or take funtion as a parameters
+// let arr=[1,2,3,4,5];
+// arr.forEach(function printval(val){
+//     console.log(val**2);
+// })
+// let arr=[1,2,3,4,5];
+// let calsqu = (arr) =>{
+//     console.log(arr**2)
+// };
+// arr.forEach(calsqu);
+//Some other Array methods 
+//Map method    arr.map(fun())    it generates new array unlike forEach method
+// let arr=[1,2,3,4,5];       using map for printing values in array
+// arr.map((val)=>{
+//     console.log(val);
+// })
+
+//using map tp print new array
+// let arr=[1,2,3,4,5];
+// let newarr = arr.map((val)=>{
+//     return val**2;
+// })
+// console.log(newarr);
+
+//filter method
+// let arr=[1,2,3,4,5];            //it generates new array unlike forEach method
+// let evenarr = arr.filter((val)=>{
+//     return val%2==0;
+// });
+// console.log(evenarr);
+
+//reduce method         summation of array elements
+// let arr=[1,2,3,4,5];
+// const sumarr=arr.reduce((res,curr)=>{
+//     return res+curr;
+// });
+// console.log(sumarr);
+
+// let arr=[1,2,3,4,5];                //max of array elements
+// const maxval=arr.reduce((res,curr)=>{
+//     return res>curr?res:curr;
+// });
+// console.log(maxval);
--- proj1.js
+++ proj1.js
@@ -1,124 +1,173 @@
-//1 project
-const Product={
-    item : "Ball pen",
-    Rating : "5 star",
-    Price : 270,
-    offer : "Deal of the day",
-}
-
-console.log(Product);
-console.log(typeof Product);
-
-
-const Profile = {
-    Name : "P.Sri Charan",
-    Followers : 50,
-    Following : 0,
-    isfolllow : true,
-}
-console.log(Profile);
-console.log(typeof Profile);
-console.log(typeof Profile["Name"]);
-console.log(typeof Profile["Following"]);
-console.log(typeof Profile["Followers"]);
-console.log(typeof Profile["isfolllow"]);
-/*Multiple lines*/
-//single line
-//2 project
-// let num=prompt("Enter a number : ");
-// if (num%5 ==0){
-//     console.log(num,"is multiple of 5");
-// }
-// else{
-//     console.log(num,"is NOT multiple of 5");
+// //1 project
+// const Product={
+//     item : "Ball pen",
+//     Rating : "5 star",
+//     Price : 270,
+//     offer : "Deal of the day",
 // }
 
-//3 project
-// let score=prompt("Enter score : ");
-// if(score<=100 && score>80){
-//     console.log("A grade");
+// console.log(Product);
+// console.log(typeof Product);
+
+
+// const Profile = {
+//     Name : "P.Sri Charan",
+//     Followers : 50,
+//     Following : 0,
+//     isfolllow : true,
 // }
-// else if(score>70 && score<=70){
-//     console.log("B grade");
+// console.log(Profile);
+// console.log(typeof Profile);
+// console.log(typeof Profile["Name"]);
+// console.log(typeof Profile["Following"]);
+// console.log(typeof Profile["Followers"]);
+// console.log(typeof Profile["isfolllow"]);
+// /*Multiple lines*/
+// //single line
+// //2 project
+// // let num=prompt("Enter a number : ");
+// // if (num%5 ==0){
+// //     console.log(num,"is multiple of 5");
+// // }
+// // else{
+// //     console.log(num,"is NOT multiple of 5");
+// // }
+
+// //3 project
+// // let score=prompt("Enter score : ");
+// // if(score<=100 && score>80){
+// //     console.log("A grade");
+// // }
+// // else if(score>70 && score<=70){
+// //     console.log("B grade");
+// // }
+// // else if(score>60 && score<=69){
+// //     console.log("C grade");
+// // }
+// // else if(score>50 && score<=59){
+// //     console.log("D grade");
+// // }
+// // else{
+// //     console.log("Fail");
+// // }
+
+// //4 project
+// for(let ii=0;ii<=100;ii++){
+//     console.log(ii);
 // }
-// else if(score>60 && score<=69){
-//     console.log("C grade");
-// }
-// else if(score>50 && score<=59){
-//     console.log("D grade");
-// }
-// else{
-//     console.log("Fail");
+// console.log("Project 5")
+// //5 Project
+// for(let jj=0;jj<=100;jj++){
+//     if(jj%2 ==0){
+//         console.log(jj);
+//     }
 // }
 
-//4 project
-for(let ii=0;ii<=100;ii++){
-    console.log(ii);
-}
-console.log("Project 5")
-//5 Project
-for(let jj=0;jj<=100;jj++){
-    if(jj%2 ==0){
-        console.log(jj);
-    }
-}
+// //6 Project
+// let game_num=25;
+// // do{
+//     // var l=prompt("Enter any number to guess game number : ");
 
-//6 Project
-let game_num=25;
-// do{
-    // var l=prompt("Enter any number to guess game number : ");
+// // }while(l!=game_num);
+// // console.log("Congratulations");
 
-// }while(l!=game_num);
-// console.log("Congratulations");
+// //7 Project
+// // name=prompt("Enter user-name : ");
+// // let lenn=name.length1;
+// // let id="@" + name + lenn;
+// // console.log(id);
 
-//7 Project
-// name=prompt("Enter user-name : ");
-// let lenn=name.length1;
-// let id="@" + name + lenn;
-// console.log(id);
-
-//5 Project
-let marks = [85,97,44,37,76,60];
-let sum=0;
-for(let val of marks){
-    sum+=val;
-}
-let avg=sum/marks.length;
-console.log(avg);
+// //5 Project
+// let marks = [85,97,44,37,76,60];
+// let sum=0;
+// for(let val of marks){
+//     sum+=val;
+// }
+// let avg=sum/marks.length;
+// console.log(avg);
 
 
-//6 Project
-let iii=0;
-ar=[250,645,300,900,50];
-for(let val of ar){
-    let offer=val/10;
-    ar[iii]=ar[iii]-offer;
-    console.log(ar[iii]);
-    iii++;
-}
+// //6 Project
+// let iii=0;
+// ar=[250,645,300,900,50];
+// for(let val of ar){
+//     let offer=val/10;
+//     ar[iii]=ar[iii]-offer;
+//     console.log(ar[iii]);
+//     iii++;
+// }
 
 
-companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflex"];
-del=companies.shift();
-console.log(companies);
-companies.splice(1,1,"Ola");
-console.log(companies);
-companies.splice(5,0,"Amazon");
-console.log(companies);
+// companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflex"];
+// del=companies.shift();
+// console.log(companies);
+// companies.splice(1,1,"Ola");
+// console.log(companies);
+// companies.splice(5,0,"Amazon");
+// console.log(companies);
 
-//No of vowels in a string using functions
-function countvowell(st){
-    let count=0;
-    for(const char of st){
-        if(char==="a" || char==="e" || char==="i" || char==="o" ||char==="u")
-            count++;
-        }
-    console.log(count);
-    }
+// //No of vowels in a string using functions
+// function countvowell(st){
+//     let count=0;
+//     for(const char of st){
+//         if(char==="a" || char==="e" || char==="i" || char==="o" ||char==="u")
+//             count++;
+//         }
+//     console.log(count);
+//     }
     
     
-//Square of number
-let nums=[1,2,3,4,5];
-nums.forEach((num)=>{
-    console.log(num*num);    
-})
+// //Square of number
+// let nums=[1,2,3,4,5];
+// nums.forEach((num)=>{
+//     console.log(num*num);    
+// })
+
+//Practice 1
+// function countval(str){
+//     let count=0;
+//     for(const char of str){
+//         if(char ==='a' || char ==='e' || char ==='i' || char ==='o' || char ==='u'){
+//            // console.log(char);
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+
+
+//Practice 2
+
+// const vow=(str)=>{
+//      let count=0;
+//      for(const char of str){
+//          if(char ==='a' || char ==='e' || char ==='i' || char ==='o' || char ==='u'){
+//             // console.log(char);
+//              count++;
+//          }
+//      }
+//      console.log(count);
+// }
+//Practice 3
+// let marks=[23,54,99,100,98,65];
+// let toppers = marks.filter((val)=>{
+//     return val>90;
+// })
+// console.log(toppers);
+
+//Practice 4
+// let n=prompt("Enter a number");
+// let arr=[];
+// for(let i=1;i<=n;i++){
+//     arr[i-1]=i;
+// }
+
+// const sumnarr=arr.reduce((res,curr)=>{
+//     return res+curr;
+// })
+// console.log(sumnarr);
+
+// const mularr=arr.reduce((res,curr)=>{
+//     return res*curr;
+// })
+// console.log(mularr);
